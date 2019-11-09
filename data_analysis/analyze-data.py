c2b78cd0a769f98de2ba8b7799ae70e0c85a38f4
agataf <agataf@stanford.edu>
2019-11-08 20:22:04 -0800

    moved scripts for data analysis to data_analysis; added function for generating pandas dataframes

--- data_analysis/analyze-data.py
+++ data_analysis/analyze-data.py
@@ -4,6 +4,7 @@ import json
 import sys
 from collections import defaultdict, Counter
 import numpy as np
+import pandas as pd
 import re
 
 from utils.fix_label import fix_general_label_error
@@ -93,6 +94,11 @@ def replace_with_slots(sentence, turn_label, label_dict):
 
     return ' '.join(sentence)
 
+def difference(new, old):
+    new_set = set(new)
+    old_set = set(old)
+    added = list(set(new_set)-set(old_set))
+    return added
 
 def load_data():
     filename = 'data/train_dials.json'
@@ -123,5 +129,35 @@ def load_data():
 
             prev_turn = turn['domain']
 
-def main():
-    for
+def generate_turn_frame(data):    
+    
+    slot_updates = dict()
+    
+    for dialogue in data:
+        
+        old_belief = []
+        old_slots = []
+        d_idx = dialogue["dialogue_idx"].split('.')[0]
+        
+        for t_idx, turn in enumerate(dialogue["dialogue"]):
+            
+            idx = '_'.join((d_idx, str(t_idx)))
+            slot_updates[idx] = dict()
+            belief = ['-'.join(el) for el in dialogue["dialogue"][t_idx]["turn_label"]]
+            slots = [el[0] for el in dialogue["dialogue"][t_idx]["turn_label"]]
+            added_belief = difference(belief, old_belief)
+            added_slots = difference(slots, old_slots)
+            
+            slot_updates[idx] = {'dialogue': d_idx, 'turn': t_idx, 'full_belief': belief, \
+                                  'step_belief': added_belief, 'full_slots': slots, 'step_slots': added_slots, \
+                                  'transcript': turn['transcript'], 'system_transcript': turn['system_transcript'], \
+                                  'step_empty': (len(added_belief) == 0), 'full_empty': (len(belief) == 0)}
+            
+            old_belief = belief
+            old_slots = slots
+            
+    slot_updates = pd.read_json(json.dumps(slot_updates)).transpose()
+    return slot_updates
+
+# def main():
+#     for
--- try.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-a = np.array([[1,2],[3,4], [5,6]])
-b = a**2
-c = np.sum(b)
-d = np.sqrt(c)
-print(d)
