07968782592e46f6a2ede933a26bb37221d18b27
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-12-01 20:34:36 -0800

    More work in clustering errors...

--- cluster-errors.py
+++ cluster-errors.py
@@ -5,6 +5,7 @@ import sys
 import copy
 import random
 import collections
+from functools import partial
 import numpy as np
 import sklearn.cluster
 import sklearn.decomposition
@@ -15,7 +16,8 @@ from utils.eval_utils import get_joint_accuracy, get_name_accuracy, print_turn, 
 from utils.augment import EXPERIMENT_DOMAINS, ALL_SLOTS
 
 ExampleTuple = collections.namedtuple('ExampleTuple',
-                                      ('dialogue', 'turn_idx', 'annotation', 'prediction', 'label', 'features'))
+                                      ('dialogue', 'turn_idx', 'annotation', 'prediction', 'previous',
+                                       'label', 'features'))
 np.random.seed(1234)
 random.seed(1236)
 
@@ -36,20 +38,16 @@ def get_value(slot_key, prediction):
     return 'none'
 
 
-def feature_uses_slot_in_annotation_or_prediction(slot_name):
-    return lambda example: (uses_slot(slot_name, example.annotation) or uses_slot(slot_name, example.prediction))
-
-
-def feature_uses_slot_in_annotation(slot_name):
-    return lambda example: (uses_slot(slot_name, example.annotation))
-
-
-def feature_slot_is_wrong(slot_name):
-    return lambda example: get_value(slot_name, example.annotation) != get_value(slot_name, example.prediction)
-
-
-def feature_has_domain(domain):
-    return lambda example: domain in example.dialogue['domains']
+def substring_seq(string, substring):
+    for i in range(len(string) - len(substring) + 1):
+        good = True
+        for j in range(len(substring)):
+            if string[i + j] != substring[j]:
+                good = False
+                break
+        if good:
+            return i
+    return -1
 
 
 ALL_FEATURES = collections.OrderedDict()
@@ -59,12 +57,108 @@ def feature(fn):
     return fn
 
 
-def feature_number_of_slots_in_annotation(num):
-    return lambda example: len(example.annotation) == num
+def per_domain_feature(fn):
+    for domain in EXPERIMENT_DOMAINS:
+        if domain == 'none':
+            continue
+        ALL_FEATURES[fn.__name__ + ':' + domain] = partial(fn, domain)
+    return fn
 
 
-def feature_turn_number(num):
-    return lambda example: example.turn_idx == num
+def per_slot_feature(fn):
+    for slot_key in ALL_SLOTS:
+        ALL_FEATURES[fn.__name__ + ':' + slot_key] = partial(fn, slot_key)
+    return fn
+
+
+def range_feature(max_num):
+    def decorator(fn):
+        for num in range(max_num):
+            ALL_FEATURES[fn.__name__ + ':' + str(num)] = partial(fn, num, exact=True)
+        ALL_FEATURES[fn.__name__ + ':>=' + str(max_num)] = partial(fn, max_num, exact=False)
+    return decorator
+
+
+#@per_slot_feature
+def uses_slot_in_annotation_or_prediction(slot_name, example):
+    return uses_slot(slot_name, example.annotation) or uses_slot(slot_name, example.prediction)
+
+
+@per_slot_feature
+def uses_slot_in_annotation(slot_name, example):
+    return uses_slot(slot_name, example.annotation)
+
+
+#@per_slot_feature
+def slot_is_wrong(slot_name, example):
+    return get_value(slot_name, example.annotation) != get_value(slot_name, example.prediction)
+
+
+@per_domain_feature
+def has_domain(domain, example):
+    return domain in example.dialogue['domains']
+
+
+@per_slot_feature
+def slot_value_appears_in_dialogue(slot_key, example):
+    slot_value = get_value(slot_key, example.annotation)
+    if slot_value in ('yes', 'no', 'dontcare', 'none'):
+        return False
+
+    slot_value = slot_value.split()
+    for turn in example.dialogue['dialogue']:
+        turn_idx = turn['turn_idx']
+        if turn_idx > example.turn_idx:
+            break
+        if substring_seq(turn['system_transcript'].split(), slot_value) != -1:
+            return True
+        if substring_seq(turn['transcript'].split(), slot_value) != -1:
+            return True
+    return False
+
+
+@per_slot_feature
+def slot_value_appears_in_user(slot_key, example):
+    slot_value = get_value(slot_key, example.annotation)
+    if slot_value in ('yes', 'no', 'dontcare', 'none'):
+        return False
+
+    slot_value = slot_value.split()
+    for turn in example.dialogue['dialogue']:
+        turn_idx = turn['turn_idx']
+        if turn_idx > example.turn_idx:
+            break
+        if substring_seq(turn['transcript'].split(), slot_value) != -1:
+            return True
+    return False
+
+
+@per_slot_feature
+def annotation_type_error(slot_key, example):
+    slot_value = get_value(slot_key, example.annotation)
+    if slot_value in ('dontcare', 'none'):
+        return False
+
+    if slot_key in ('hotel-parking', 'hotel-internet'):
+        return slot_value not in ('yes', 'no')
+
+    return slot_value in ('yes', 'no')
+
+
+@range_feature(10)
+def number_of_slots_in_annotation(num, example, exact=True):
+    if exact:
+        return len(example.annotation) == num
+    else:
+        return len(example.annotation) >= num
+
+
+@range_feature(5)
+def turn_number(num, example, exact=True):
+    if exact:
+        return example.turn_idx == num
+    else:
+        return example.turn_idx >= num
 
 
 #@feature
@@ -73,19 +167,27 @@ def correct_slot_names(example):
 
 
 #@feature
-def annotation_is_subset(error):
-    for slot in error.annotation:
-        if not slot in error.prediction:
+def annotation_is_subset(example):
+    for slot in example.annotation:
+        if not slot in example.prediction:
             return False
     return True
 
 
 #@feature
-def prediction_is_subset(error):
-    for slot in error.prediction:
-        if not slot in error.annotation:
+def prediction_is_subset(example):
+    for slot in example.prediction:
+        if not slot in example.annotation:
             return False
     return True
+
+
+@feature
+def previous_turn_correct(example):
+    if example.previous is None:
+        return True
+    else:
+        return example.previous.label > 0.0
 
 
 @feature
@@ -98,34 +200,6 @@ def has_dontcare(example):
 def has_boolean(example):
     #return any(slot.endswith('-yes') or slot.endswith('-no') for slot in example.prediction) or \
     return any(slot.endswith('-yes') or slot.endswith('-no') for slot in example.annotation)
-
-
-def make_turn_number_features(max_turn_number):
-    for turn_idx in range(max_turn_number):
-        ALL_FEATURES['turn_number:' + str(turn_idx)] = feature_turn_number(turn_idx)
-    ALL_FEATURES['turn_number:>=' + str(max_turn_number)] = lambda example: example.turn_idx >= max_turn_number
-
-
-def make_number_of_slots_in_annotation_feature(max_num_slots):
-    for num_slots in range(max_num_slots):
-        ALL_FEATURES['number_of_slots_in_annotation:' + str(num_slots)] = \
-            feature_number_of_slots_in_annotation(num_slots)
-    ALL_FEATURES['number_of_slots_in_annotation:>=' + str(max_num_slots)] = \
-        lambda example: len(example.annotation) >= max_num_slots
-
-
-def load_features():
-    for slot_key in ALL_SLOTS:
-        #ALL_FEATURES['uses_slot_in_annotation_or_prediction:' + slot_key] = feature_uses_slot_in_annotation_or_prediction(slot_key)
-        #ALL_FEATURES['slot_is_wrong:' + slot_key] = feature_slot_is_wrong(slot_key)
-        ALL_FEATURES['uses_slot_in_annotation:' + slot_key] = feature_uses_slot_in_annotation(slot_key)
-    for domain in EXPERIMENT_DOMAINS:
-        if domain == 'none':
-            continue
-        ALL_FEATURES['has_domain:' + domain] = feature_has_domain(domain)
-    make_turn_number_features(5)
-    make_number_of_slots_in_annotation_feature(10)
-load_features()
 
 
 def load_data():
@@ -152,6 +226,7 @@ def load_predictions(all_models):
 def compute_examples(dialogue_dev_data, pred_data):
     examples = []
 
+    previous = None
     for dialogue_id, dialogue in dialogue_dev_data.items():
         for turn in dialogue['dialogue']:
             turn_idx = turn['turn_idx']
@@ -162,20 +237,25 @@ def compute_examples(dialogue_dev_data, pred_data):
             prediction.sort()
 
             joint_accuracy = get_joint_accuracy(pred_data[dialogue_id][turn_idx_str])
-            examples.append(ExampleTuple(dialogue, turn_idx, annotation, prediction,
-                                         label=joint_accuracy, features=dict()))
+
+            example = ExampleTuple(dialogue, turn_idx, annotation, prediction,
+                                   previous=previous, label=joint_accuracy, features=dict())
+            examples.append(example)
+            previous = example
 
     return examples
 
 
 def featurize_examples(examples):
-    X = np.zeros((len(examples), len(ALL_FEATURES)), dtype=np.float32)
+    X = np.zeros((len(examples), 2*len(ALL_FEATURES)), dtype=np.float32)
     Y = np.empty((len(examples),), dtype=np.float32)
 
     for example_idx, example in enumerate(examples):
         for feature_idx, (feature_name, feature_func) in enumerate(ALL_FEATURES.items()):
             example.features[feature_name] = feature_func(example)
             X[example_idx, feature_idx] = float(example.features[feature_name])
+            example.features['!' + feature_name] = 1 - X[example_idx, feature_idx]
+            X[example_idx, len(ALL_FEATURES) + feature_idx] = 1 - X[example_idx, feature_idx]
             Y[example_idx] = float(example.label)
 
     #for error in errors[:10]:
@@ -266,25 +346,25 @@ def do_classifier(dialogue_dev_data, pred_data, examples, example_features, exam
     print()
     print(classifier.score(example_features, example_labels))
 
-    for feature_idx, feature in enumerate(ALL_FEATURES):
+    all_features = list(ALL_FEATURES) + ['!' + feature for feature in ALL_FEATURES]
+    for feature_idx, feature in enumerate(all_features):
         print(f'{feature} = {classifier.coef_[0, feature_idx]}')
 
 
 def do_heuristic(dialogue_dev_data, pred_data, examples, example_features, example_labels):
+    all_features = list(ALL_FEATURES) + ['!' + feature for feature in ALL_FEATURES]
     feature_counts = np.sum(example_features, axis=0)
-    feature_error_count = np.zeros((len(ALL_FEATURES),), dtype=np.float32)
+    feature_error_count = np.zeros((len(all_features),), dtype=np.float32)
 
     for example_idx, example in enumerate(examples):
         if example_labels[example_idx] < 1.0:
             feature_error_count += example_features[example_idx]
 
-    feature_error_percent = feature_error_count / feature_counts
+    feature_error_percent = feature_error_count / (1e-5 + feature_counts)
 
     feature_error_percent = [(feature_idx, feature_error_percent[feature_idx], feature_counts[feature_idx])
-                             for feature_idx in range(len(ALL_FEATURES))]
+                             for feature_idx in range(len(all_features))]
     feature_error_percent.sort(key=lambda x: (x[1], x[2]))
-
-    all_features = list(ALL_FEATURES)
 
     for feature_idx, error_pct, feature_cnt in feature_error_percent:
         print('%s = %.0f / %.0f = %.2f' % (all_features[feature_idx],
@@ -294,14 +374,14 @@ def do_heuristic(dialogue_dev_data, pred_data, examples, example_features, examp
 
 
 def do_greedy(dialogue_dev_data, pred_data, examples):
-    all_features = list(ALL_FEATURES)
+    all_features = list(ALL_FEATURES) + ['!' + feature for feature in ALL_FEATURES]
 
     examples_total = len(examples)
     examples_left = examples_total
     cluster_idx = 0
-    while examples_left / examples_total > 0.1:
-        feature_counts = np.zeros((len(ALL_FEATURES),), dtype=np.float32)
-        feature_error_count = np.zeros((len(ALL_FEATURES),), dtype=np.float32)
+    while examples_left / examples_total > 0.05:
+        feature_counts = np.zeros((len(all_features),), dtype=np.float32)
+        feature_error_count = np.zeros((len(all_features),), dtype=np.float32)
 
         for example_idx, example in enumerate(examples):
             for feature_idx, feature in enumerate(all_features):
@@ -334,8 +414,8 @@ def do_count_features(example_features):
 
 
 def main():
-    #modelname = 'baseline21'
-    modelname = 'aug5'
+    modelname = 'baseline21'
+    #modelname = 'aug5'
 
     dialogue_dev_data = load_data()
     pred_data_all = load_predictions([modelname])
